7a40630dfc17051f37b06afedf418c691897e9c3
Nathan Levitt <nathan@trudine.com>
2024-01-25 16:52:57 -0800

    fix: navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,15 +2,17 @@ import Image from "next/image";
 
 export function Navbar() {
   return (
-    <div className="max-w-xl px-4 py-4 mx-auto">
-      <div className="flex items-center justify-between flex-1">
-        <Image
-          src={"/logo.png"}
-          width={45}
-          height={45}
-          alt="Nathan Levitt"
-          className="border rounded-2xl border-neutral-200 dark:border-neutral-700"
-        />
+    <div className="fixed top-0 left-0 right-0">
+      <div className="max-w-xl px-4 py-4 mx-auto">
+        <div className="flex items-center justify-between flex-1">
+          <Image
+            src={"/logo.png"}
+            width={45}
+            height={45}
+            alt="Nathan Levitt"
+            className="border rounded-2xl border-neutral-200 dark:border-neutral-700"
+          />
+        </div>
       </div>
     </div>
   );
